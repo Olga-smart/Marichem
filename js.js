b12433ea62b108f11ea2e4728417c56830f0b397
olga-smart <olgat.smart@gmail.com>
2021-03-08 18:21:29 +0700

    Add loguc for .product-card__video-btn

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ const burger = document.querySelector('.navbar__burger')
 const footer = document.querySelector('.page-footer');
 let footerHeight = footer.offsetHeight;
 
-// Make Search in Nav to Work
+// Make search in nav to work
 search.addEventListener('click', function(e) {
   e.stopPropagation();
   this.classList.add('search_active');
@@ -14,13 +14,13 @@ search.addEventListener('click', function(e) {
 body.addEventListener('click', function() {
   search.classList.remove('search_active');
 });
-// End Make Search in Nav to Work
+// End Make search in nav to work
 
-
+// Toggle burger class for animation
 burger.addEventListener('click', function() {
   burger.classList.toggle('navbar__burger_menu_open');
 });
-
+// End Toggle burger class for animation
 
 // Fix footer on the bottom of the page
 document.addEventListener('DOMContentLoaded', function() {
@@ -33,4 +33,24 @@ window.addEventListener('resize', function() {
   body.style.paddingBottom = footerHeight + 'px';
 });
 // End Fix footer on the bottom of the page
+
+// Make video in modal on product page to work
+let videoModal = document.querySelector('#videoModal');
+
+if (videoModal) {
+  
+  let modalIframe = videoModal.querySelector('iframe');
+  
+  // Pass src from button to modal
+  videoModal.addEventListener('show.bs.modal', function(event) {
+    let button = event.relatedTarget;
+    modalIframe.setAttribute('src', button.dataset.src);
+  });
+  
+  // Stop playing video when modal is closed
+  videoModal.addEventListener('hide.bs.modal', function() {
+    modalIframe.setAttribute('src', '');
+  });
+}
+// End Make video in modal on product page to work
 
